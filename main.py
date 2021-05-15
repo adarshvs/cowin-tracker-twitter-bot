1a09be61fe43cdcf10828f989817af55b59df208
adarshvs <mannarakaladarsh@gmail.com>
2021-05-15 13:15:00 +0530

    changed image template format and authentication key read format
    - added time slot to image template

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ import json
 import tweepy
 from PIL import Image, ImageDraw, ImageFont
 
-# telegram authentication token
-token = 'your bot token'
-channel_id = 'your channel name'
-# authentication of access token and secret
-consumer_key = 'consumer key'
-consumer_secret = 'consumer secrets'
-access_token = 'access token'
-access_token_secret = 'access token secret'
+with open("settings.json") as f:
+    settings = json.load(f)
+
+# telegram authentication goes here
+token = settings["telegramToken"]
+channel_id = settings["channelId"]
+# Twitter authentication goes here
+consumer_key = settings["consumerKey"]
+consumer_secret = settings["consumerSecret"]
+access_token = settings["twitterAccessToken"]
+access_token_secret = settings["accessTokenSecret"]
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -26,11 +29,11 @@ font = ImageFont.truetype('font/Roboto-Bold.ttf', size=80)
 avail_date_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=40)
 count_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=60)
 vaccine_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=60)
-dist_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=40)
+dist_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=50)
+slot_font = ImageFont.truetype('font/Roboto-Bold.ttf', size=50)
+
 
 # get current date
-
-
 def getDate():
     current_time = datetime.datetime.now()
     day = current_time.day
@@ -40,9 +43,9 @@ def getDate():
     # set time interval for checking vaccine availability, default time : 15 minutes
     time.sleep(900)
     return date
+
+
 # get name of district
-
-
 def getDistrictID(st_id, lookout_district):
     district_name = ''
     url = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/{st}".format(
@@ -70,9 +73,8 @@ def getCOWIN(date, district_id):
     response = requests.get(url, headers=headers)
     return json.loads(response.text)
 
+
 # check slot availability
-
-
 def checkAvailability(payload):
     if('centers' in payload.keys()):
         length = len(payload['centers'])
@@ -85,15 +87,16 @@ def checkAvailability(payload):
                         res = {'available_center': payload['centers'][i]['name'],
                                'capacity': payload['centers'][i]['sessions'][j]['available_capacity'],
                                'vaccine': payload['centers'][i]['sessions'][j]['vaccine'],
-                               'date_avail': payload['centers'][i]['sessions'][j]['date']}
+                               'date_avail': payload['centers'][i]['sessions'][j]['date'],
+                               'slots': payload['centers'][i]['sessions'][j]['slots']}
                         output.append(res)
 
     return output
 
 
 while True:
-    st_id = 17
-    lookout_district = 296  # distict id which you want to keep an eye on vaccine availability
+    st_id = 17  # State id which you want to keep an eye on vaccine availability
+    lookout_district = 300  # distict id which you want to keep an eye on vaccine availability
     date = getDate()
     district_id = lookout_district
     dist_name = getDistrictID(st_id, lookout_district)
@@ -105,44 +108,51 @@ while True:
             ") \n #getvaccinated #cowin  #CrushTheCurve \n posted by:#cowintrackerbot"
         image = Image.open('images/cowin.jpg')
         draw = ImageDraw.Draw(image)
-        (x, y) = (122, 170)
+        (x, y) = (16, 92)
         center_name = key['available_center']
         color = 'rgb(255, 255, 255)'  # white
         draw.text((x, y), center_name, fill=color, font=font)
 
-        (x, y) = (1184, 65)
+        (x, y) = (1140, 21)
         avail_date = key['date_avail']
         color = 'rgb(0, 0, 0)'  # black color
         draw.text((x, y), avail_date, fill=color, font=avail_date_font)
 
-        (x, y) = (203, 363)
+        (x, y) = (47, 271)
         dose = str(key['capacity'])
         color = 'rgb(0, 0, 0)'  # black color
         draw.text((x, y), dose, fill=color, font=count_font)
 
-        (x, y) = (197, 492)
+        (x, y) = (43, 398)
         age_lim = "45"
         color = 'rgb(0, 0, 0)'  # black color
         draw.text((x, y), age_lim, fill=color, font=count_font)
 
-        (x, y) = (382, 609)
+        (x, y) = (607, 396)
         vaccine_name = key['vaccine']
         color = 'rgb(139,0,0)'  # maroon color
         draw.text((x, y), vaccine_name, fill=color, font=vaccine_font)
 
-        (x, y) = (1054, 134)
+        (x, y) = (611, 263)
         dist_names = dist_name
-        color = 'rgb(48,8,8)'  # black color
+        color = 'rgb(7,38,56)'  # black color
         draw.text((x, y), dist_names, fill=color, font=dist_font)
+
+        (x, y) = (535, 519)
+        slot_avail = '\n'.join(key['slots'])
+        color = 'rgb(7,38,56)'  # black color
+        draw.text((x, y), slot_avail, fill=color, font=slot_font)
 
         image.save('images/optimized.png', optimize=True, quality=20)
         img_path = 'images/optimized.png'
+        # Twitter post section goes here
         api.update_with_media(img_path, status)
 
         # telegram post section goes here
         cap_text = str(key['capacity']) + " doses of "+key['vaccine'] + \
-            " available at "+key['available_center']+" %23"+dist_name
+            " available at "+key['available_center'] + \
+            " %23"+dist_name+" ("+avail_date+")"
         tel_files = {'photo': open(img_path, 'rb')}
-        resps = requests.post('https://api.telegram.org/bot'+token+'/sendPhoto?chat_id=@{}'.format(
+        requests.post('https://api.telegram.org/bot'+token+'/sendPhoto?chat_id=@{}'.format(
             channel_id)+'&caption={}'.format(cap_text), files=tel_files)
-        print(resps)
+        print(center_name)
